e073a3940199f6d795e264a3488865deb623e3e2
Buyandelger Batsaikhan <buya@metawise.net>
2024-09-17 13:41:02 -0400

    title update

--- components/app-page.tsx
+++ components/app-page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { ClipboardCopy, Instagram } from "lucide-react"
-import { ScrollArea } from "@/components/ui/scroll-area"
+// import { ScrollArea } from "@/components/ui/scroll-area"
 
 type DisplayFormat = 'plain' | 'comma' | 'json'
 
